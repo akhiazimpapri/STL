dcefb9ba80b95d5b1ba35e673c12959d0210c612
Akhi Azim Papri <akhi@Akhis-MacBook-Air.local>
2025-05-02 13:23:40 +0600

    final

---
+++ set.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<set>
+using namespace std;
+
+int main() {
+    set<int>s;
+
+    s.insert(1);
+    s.insert(2);
+    s.insert(3);
+    s.insert(4);
+    s.insert(5);
+    s.insert(6);
+
+    s.insert(1);
+    s.insert(2);
+    s.insert(3);
+
+    cout<<"size = "<<s.size()<<endl;
+
+    for(auto val : s) {
+        cout<<val<<" ";
+    }
+    cout << endl;
+
+    return 0;
+}
+
